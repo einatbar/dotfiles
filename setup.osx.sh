698b93d2d87c2b56862c149505a535848a94b5f6
Ori Riner <OriR@users.noreply.github.com>
2020-03-11 12:50:56 +0200

    Add sudo keep-alive for main osx setup script

--- setup.osx.sh
+++ setup.osx.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 hash brew 2>/dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 hash chezmoi 2>/dev/null || brew install twpayne/taps/chezmoi
