fc132354ed82d9c1e976e6d7fff3e16840ca91b0
Ori Riner <OriR@users.noreply.github.com>
2020-03-13 00:09:30 +0200

    Add custom cheats path to navi

--- environments/executable_shell.sh
+++ environments/executable_shell.sh
@@ -18,3 +18,7 @@ bindkey '^[[B' history-substring-search-down
 
 # Adding navi as a custom keybinding instead of running the command, default keybinding is Ctrl+g
 source "$(navi widget zsh)"
+
+# Since navi doesn't support well-known configuration folders and only allows to have custom that override all builtin cheats.
+# This line adds the builtin cheats AND a custom cheats folder found in ~/cheats
+export NAVI_PATH="$(navi home)/../libexec/cheats:$HOME/cheats"
